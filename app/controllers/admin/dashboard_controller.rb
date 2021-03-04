98d8b1a1119413febdcea060f62f14430003ca41
Mike Ackison <mike.ackison@shopify.com>
2021-03-04 00:52:56 +0000

    add stats on dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
   protected
