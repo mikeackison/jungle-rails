c6a824dc345a1ebe662cc99f8f7b42a385819df9
Mike Ackison <mike.ackison@shopify.com>
2021-03-04 04:31:32 +0000

    list out categories, basic logic

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,29 @@
 class Admin::CategoriesController < ApplicationController
+  before_filter :authenticate
+
   def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  # def new
+  #   @category = Category.new
+  # end
+
+  # def create
+  #   @category = Category.new(category_params)
+
+  #   if @category.save
+  #     redirect_to [:admin, :categories], notice: "Category created!"
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["USERNAME"] && password == ENV["PASSWORD"]
+    end
   end
 end
