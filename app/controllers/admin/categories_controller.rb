e318bf374bec2c1aa75f47ac3c3f7062cca2a220
Mike Ackison <mike.ackison@shopify.com>
2021-03-04 15:05:53 +0000

    use logic from products and adpt for categories, pave views workinng, logic needs to be implemented

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,19 +5,19 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.order(id: :desc).all
   end
 
-  # def new
-  #   @category = Category.new
-  # end
+  def new
+    @category = Category.new
+  end
 
-  # def create
-  #   @category = Category.new(category_params)
+  def create
+    @category = Category.new(category_params)
 
-  #   if @category.save
-  #     redirect_to [:admin, :categories], notice: "Category created!"
-  #   else
-  #     render :new
-  #   end
-  # end
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
+  end
 
   protected
 
