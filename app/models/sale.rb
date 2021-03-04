d4e74cf6e10e01be661686d9bf29129a6dccfddc
Mike Ackison <mike.ackison@shopify.com>
2021-03-04 23:40:50 +0000

    add logic for sales active, upcoming, or finished bageds

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,13 @@
 class Sale < ActiveRecord::Base
+  def finished?
+    self.ends_on < Date.current
+  end
+
+  def upcoming?
+    self.starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end
