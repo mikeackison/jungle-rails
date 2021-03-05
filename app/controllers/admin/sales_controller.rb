1755937238fefb7d8a3d4da2e4165773e50b85e2
Mike Ackison <mike.ackison@shopify.com>
2021-03-05 17:06:44 +0000

    add auth

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,6 +1,14 @@
 class Admin::SalesController < ApplicationController
+  before_filter :authenticate
+
   def index
     @sales = Sale.all
     @active_sale = Sale.active.first
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["USERNAME"] && password == ENV["PASSWORD"]
+    end
   end
 end
