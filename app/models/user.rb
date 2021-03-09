e41a4746c5fac58e8bbeda9f504fca6ee42ead0a
Mike Ackison <mike.ackison@shopify.com>
2021-03-09 04:08:57 +0000

    add authenticate_with_credentials and tests and everything seems to be working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :first_name, :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+
+    if (user && user.authenticate(password))
+      user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,12 +15,13 @@ RSpec.describe User, type: :model do
       expect(errors[0]).to include("Password confirmation doesn't match Password")
     end
 
-    it "should not validate an new user when passwords are blank" do
+    it "should not validate a new user when passwords are blank" do
       user = User.new(first_name: "Mike", last_name: "Ackison", email: "m@a.com", password: "", password_confirmation: "")
       user.valid?
+      expect(user).to_not be_valid
       errors = user.errors.full_messages
-      expect(errors.length).to be(1)
-      expect(errors[0]).to include("Password can't be blank")
+
+      expect(errors[1]).to include("Password can't be blank")
     end
 
     it "should not validate if email is not unique" do
@@ -48,6 +49,53 @@ RSpec.describe User, type: :model do
       expect(errors.length).to be(2)
       expect(errors[0]).to include("First name can't be blank")
       expect(errors[1]).to include("Last name can't be blank")
+    end
+
+    it "should require a password with a minimum length" do
+      user = User.new(first_name: "Mike", last_name: "Ackison", email: "m@a.com", password: "pass", password_confirmation: "pass")
+      user.valid?
+      errors = user.errors.full_messages
+      expect(errors.length).to be(1)
+      expect(errors[0]).to include("Password is too short (minimum is 5 characters)")
+    end
+
+    describe ".authenticate_with_credentials" do
+      # examples for this class method here
+      it "should log in the user if authentification is correct" do
+        user1 = User.new(first_name: "Mike", last_name: "Ackison", email: "m@a.com", password: "password", password_confirmation: "password")
+
+        user1.save
+
+        user2 = User.authenticate_with_credentials("m@a.com", "password")
+        expect(user1).to eql(user2)
+      end
+
+      it "should not log in the user if authentification is incorrect" do
+        user1 = User.new(first_name: "Mike", last_name: "Ackison", email: "m@a.com", password: "password", password_confirmation: "password")
+
+        user1.save
+
+        user2 = User.authenticate_with_credentials("m@a.com", "banana")
+        expect(user1).to_not eql(user2)
+      end
+
+      it "should log in the user if authentification correct but there are whitespaces in the email feild" do
+        user1 = User.new(first_name: "Mike", last_name: "Ackison", email: "m@a.com", password: "password", password_confirmation: "password")
+
+        user1.save
+
+        user2 = User.authenticate_with_credentials("     m@a.com    ", "password")
+        expect(user1).to eql(user2)
+      end
+
+      it "should log in the user if authentification correct but case is sensitive" do
+        user1 = User.new(first_name: "Mike", last_name: "Ackison", email: "m@a.com", password: "password", password_confirmation: "password")
+
+        user1.save
+
+        user2 = User.authenticate_with_credentials("m@a.COM", "password")
+        expect(user1).to eql(user2)
+      end
     end
   end
 end
