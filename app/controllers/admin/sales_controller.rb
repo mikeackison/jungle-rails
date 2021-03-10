a435a8a280a08da48ceb3db9992aa2f660bfe864
Mike Ackison <mike.ackison@shopify.com>
2021-03-10 01:31:42 +0000

    add routes for sales and logic to create, and destroy sales, everything seems to be working and all tests passing

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,9 +1,38 @@
 class Admin::SalesController < ApplicationController
   before_filter :authenticate
 
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: "Sale created!"
+    else
+      render :new
+    end
+  end
+
   def index
     @sales = Sale.all
     @active_sale = Sale.active.first
+  end
+
+  def destroy
+    @sale = Sale.find params[:id]
+    @sale.destroy
+    redirect_to [:admin, :sales], notice: "Sale deleted!"
+  end
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :percent_off,
+      :starts_on,
+      :ends_on
+    )
   end
 
   def authenticate
